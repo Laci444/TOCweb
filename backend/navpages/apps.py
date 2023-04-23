259b6dcbf1cd591087ef0da945e673203fb3c7b5
Laci444 <szilagyilacko1@gmail.com>
2023-04-24 00:37:06 +0200

    Revert "init"
    This reverts commit 5a40c0cf14a621c5b62a1a6df5b18ff90622c7be.

--- backend/navpages/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class NavpagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'navpages'
-    verbose_name = 'Oldalkezelő'
